28d2b0e56beee76ae982f4055a4d7172a6b9b969
Johan Remy <johan.remy@graines-digitales.online>
2022-09-03 16:10:34 +0100

    add nelmio ocrs

---
+++ src/Controller/Web/NuxtController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Web;
+
+use App\WebContent\SEO;
+use App\WebContent\WebPage;
+use App\WebContent\MetaData;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class NuxtController extends AbstractController
+{
+    public function build(): JsonResponse
+    {
+        $kernelProjectDir = $this->getParameter('kernel.project_dir');
+        $response = [];
+        $command = $kernelProjectDir.'/build.sh';
+        
+        $process = new Process(
+            [$command, $kernelProjectDir]
+        );
+        // $process->setTimeout(10800); // 3 heures
+        try {
+            $process->mustRun();
+            $response['success'] = $process->getOutput();
+        } catch (ProcessFailedException $exception) {
+            $response['error'] = $exception->getMessage();
+        }
+        return new JsonResponse(
+            $response,
+            JsonResponse::HTTP_OK
+        );
+    }
+}
