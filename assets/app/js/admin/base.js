7b954b0f1297a9cf5ff4ff06870c90c4c9004107
Johan Remy <johan.remy@graines-digitales.online>
2022-10-17 18:56:33 +0100

    add console

--- assets/app/js/admin/base.js
+++ assets/app/js/admin/base.js
@@ -16,7 +16,7 @@ const initComponent = () => {
       mutationObserver.observe(collectionList, {
         childList: true,
         attributes: true,
-        subtree: false
+        subtree: true
       })
     })
   }
@@ -37,7 +37,7 @@ const initObserver = () => {
       richContainer.forEach(element => mutationObserver.observe(element, {
         childList: true,
         attributes: true,
-        subtree: false
+        subtree: true
       }))
     })
   })
@@ -57,7 +57,7 @@ const initObserver = () => {
           richContainer.forEach(element => mutationObserver.observe(element, {
             childList: true,
             attributes: true,
-            subtree: false
+            subtree: true
           }))
         })
       })
