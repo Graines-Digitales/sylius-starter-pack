3ff227761c8c778ac3a5148d6f9cdc53dab72c79
Johan Remy <johan.remy@graines-digitales.online>
2022-10-20 13:31:20 +0100

    add slect2 to collections

--- assets/app/js/admin/base.js
+++ assets/app/js/admin/base.js
@@ -1,155 +1,45 @@
 import '../../styles/admin/base.scss';
 
-/**
- * START Région Accordion elements
- * Permet d'initialiser l'accordion de Semantic UI afin que les éléments dans les composants MonsieurBizz puissent se collapser
- */
 
-const initComponent = () => {
-  console.log('initComponent')  
-  initSelect2()
-  // initAccordion()
- 
-  
-}
-
-const initObserver = () => {
-  
-  const editButtons = document.querySelectorAll('.js-uie-edit')
-  console.log('editButtons')
-  console.log(editButtons)
-  editButtons.forEach(button => {
-    button.addEventListener('click', function handleClick(event) {
-      console.log('editButtons click')
-      console.log(button)
-      let richContainer = document.querySelectorAll('.uie-panels__new.js-uie-panels-new')
-      let mutationObserver1 = new MutationObserver(initComponent)
-      richContainer.forEach(element => mutationObserver1.observe(element, {
-        childList: true,
-        attributes: true,
-        subtree: false
-      }))
-      let mutationObserver2 = new MutationObserver(initSelect2ForCollection)
-      richContainer.forEach(element => mutationObserver2.observe(element, {
-        childList: true,
-        attributes: true,
-        subtree: false
-      }))
-    })
-  })
-
-  const addButtons = document.querySelectorAll('.js-uie-add')
-  console.log('addButtons')
-  console.log(addButtons)
-  addButtons.forEach(button => {
-    button.addEventListener('click', function handleClick(event) {
-      console.log('addButtons click')
-      console.log(button)
-      const cards = document.querySelectorAll('.js-uie-panels-selector .link.uie-card')
-      cards.forEach(card => {
-        card.addEventListener('click', function handleClick(event) {
-
-          let richContainer = document.querySelectorAll('.uie-panels__new.js-uie-panels-new')
-
-          let mutationObserver1 = new MutationObserver(initComponent)
-          richContainer.forEach(element => mutationObserver1.observe(element, {
-            childList: true,
-            attributes: true,
-            subtree: false
-          }))
-          let mutationObserver2 = new MutationObserver(initSelect2ForCollection)
-          richContainer.forEach(element => mutationObserver2.observe(element, {
-            childList: true,
-            attributes: true,
-            subtree: false
-          }))
-        
-        })
-      })
-    })
-  })
-}
+// const initComponent = () => {
+//   console.log('initComponent')  
+//   initSelect2()
+//   // initAccordion()
+// }
 
 const initAccordion = () => {
   $('.ui.accordion_alt').accordion()
 }
 
-const initSelect2ForCollection = () => {
-  const addCollectionButton = document.querySelector('#component_cards_cards [data-form-collection="add"]')
-  console.log('addCollectionButton')
-  console.log(addCollectionButton)
-  if(null !== addCollectionButton) {
-     // buttons.forEach(button => {
-      addCollectionButton.addEventListener('click', function handleClick(event) {
-        console.log('click')
-        const collectionLists = document.querySelectorAll('#component_cards_cards [data-form-collection="item"]')
-        var lastElement = collectionLists[collectionLists.length - 1]
-        if(typeof lastElement === "undefined") {
-          const selector = '#component_cards_cards [data-form-collection-index="0"]'
-          initSelect2(selector)
-        } else {
-          const currentIndex = parseInt(lastElement.dataset.formCollectionIndex)
-          const newIndex = currentIndex + 1
-          const selector = '#component_cards_cards [data-form-collection-index="' + newIndex +'"]'
-          initSelect2(selector)
-        }
-    })
-  }
-}
-
-const initSelect2 = (selector = null) => {
-  
-  if(null !== selector) {
-    $(() => {
-      console.log('initSelect2 selector')
-      $(selector + ' .select2-image').select2({
+const initSelect2 = () => {
+  $('.select2-image').each(function( index ) {
+    const elem = $( this );
+    const attr = elem.attr('data-select2-id');
+    if (typeof attr === 'undefined' || attr === false) {
+      elem.select2({
         templateResult: formatState,
         templateSelection: formatState
-      })
-      $(selector + ' .select2-icon').select2({
+      });
+    }
+  });
+  $('.select2-icon').each(function( index ) {
+    const elem = $( this );
+    const attr = elem.attr('data-select2-id');
+    if (typeof attr === 'undefined' || attr === false) {
+      elem.select2({
         templateResult: formatState2,
         templateSelection: formatState2
-      })
-      $(selector + ' .select2-standard').select2()
-    })
-  } else {
-    console.log('initSelect2')
-    $(() => {
-      $('.select2-image').select2({
-        templateResult: formatState,
-        templateSelection: formatState
-      })
-      $('.select2-icon').select2({
-        templateResult: formatState2,
-        templateSelection: formatState2
-      })
-      $('.select2-standard').select2()
-    })
-  }
-  
+      });
+    }
+  });
+  $('.select2-standard').each(function( index ) {
+    const elem = $( this );
+    const attr = elem.attr('data-select2-id');
+    if (typeof attr === 'undefined' || attr === false) {
+      elem.select2();
+    }
+  });
 }
-
-/**
- * END Région Accordion elements
- */
-
-/**
- * START Région Select2
- */
-$(document).ready(function () {
-  initSelect2()
-})
-
-// $(document).ready(function () {
-//   $('.select2-icon').select2({
-//     templateResult: formatState2,
-//     templateSelection: formatState2
-//   });
-// });
-
-// $(document).ready(function () {
-//   $('.select2-standard').select2();
-// });
 
 
 const formatState = (opt) => {
@@ -190,16 +80,18 @@ const formatState2 = (opt) => {
   ;
 });
 
-
-
-let componentsContainerObserver = new MutationObserver(initObserver)
-let componentsContainer = document.querySelectorAll('.components-container')
+let componentsContainerObserver = new MutationObserver(initSelect2)
+let componentsContainer = document.querySelectorAll('body')
 componentsContainer.forEach(element => {
   console.log('componentsContainerObserver')
   console.log(element)
   componentsContainerObserver.observe(element, {
     childList: true,
-    attributes: true,
+    attributes: false,
     subtree: true,
   })
 })
+
+jQuery(function() {
+  initSelect2()
+});
