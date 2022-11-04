e5369921a86542617c6038037122d95bd97a38a3
root <root@vps552403.ovh.net>
2022-11-04 20:57:34 +0100

    update from server

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 today=`date +%Y-%m-%d.%Hh%M`
 
-directory_prod_app=/home/www/graines-digitales/kazen-garden/nuxt3-modern-website
-directory_prod_api=/home/www/graines-digitales/kazen-garden/data-json-schemaorg-format-from-apis
+directory_prod_app=/var/www/kazen-garden/production/nuxt-modern-website
+directory_prod_api=/var/www/kazen-garden/production/data-json-schemaorg-format-from-apis
 
 cd $directory_prod_api
 node --experimental-json-modules ./api_sylius.js
