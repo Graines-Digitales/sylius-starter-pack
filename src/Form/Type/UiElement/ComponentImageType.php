b1cc9c3284568fddcc94ba101484df849f00a6ec
Johan Remy <johan.remy@graines-digitales.online>
2022-11-14 20:31:12 +0100

    add new components

---
+++ src/Form/Type/UiElement/ComponentImageType.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type\UiElement;
+
+use App\Entity\Category;
+use App\WebContent\Component;
+use App\Entity\MediaObjectIcon;
+use App\Entity\MediaObjectImage;
+use App\Entity\MediaObjectVideo;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\AbstractType;
+use App\Form\DataTransformer\TagsTransformer;
+use App\Form\Type\UiElement\ComponentLinkType;
+use Symfony\Component\Form\FormBuilderInterface;
+use App\Form\DataTransformer\CategoryTransformer;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Form\DataTransformer\MediaObjectIconTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Form\DataTransformer\MediaObjectImageTransformer;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use MonsieurBiz\SyliusRichEditorPlugin\Form\Type\WysiwygType;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+
+class ComponentImageType extends AbstractType
+{
+    private $manager;
+    
+    private $container;
+
+    private $slugger;
+
+    private $componentService;
+
+    public function __construct(
+        EntityManagerInterface $manager,
+        ContainerInterface $container,
+        Component $componentService
+    ){
+        $this->manager = $manager;
+        $this->container = $container;
+        $this->componentService = $componentService;
+        $this->slugger = new AsciiSlugger();
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $views = $this->componentService->getComponentViews();
+        
+        $builder
+            ->add('_slug', TextType::class, [
+                'disabled' => true,
+                'data' => (isset($options['data']['slug']))? $options['data']['slug']: '',
+                'label' => 'app.ui_element.field.slug',
+                'mapped' => false,
+                'help' => 'This field will be automatically edited',
+                'required' => false,
+            ])
+            ->add('slug', HiddenType::class, [
+                'disabled' => false,
+            ])
+            ->add('designation', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['groups' => ['component_contact_form_validation']])
+                ],
+                'label' => 'app.ui_element.field.designation',
+            ])
+            ->add('image', EntityType::class, [
+                'required' => false,
+                'class' => MediaObjectImage::class,
+                'placeholder' => 'app.ui_element.field.choose',
+                'attr' => ['class' => 'select2-image'],
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.updatedAt', 'DESC')
+                    ;
+                }
+            ])
+          
+        ;
+
+        $builder
+            ->get('image')
+            ->addModelTransformer(new MediaObjectImageTransformer($this->manager))
+        ;
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event): void {
+            $data = $event->getData();
+            $form = $event->getForm();
+            if(empty($data['slug'])) {
+                $string = $form->getConfig()->getName() . ' ' . $data['designation'];
+                $data['slug'] = $this->slugger->slug($string)->lower()->toString();
+            }
+            unset($data['_slug']);
+            $event->setData($data);
+        });
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'validation_groups' => ['component_image_validation'],
+        ]);
+    }
+}
---
+++ src/Form/Type/UiElement/ComponentImagesType.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type\UiElement;
+
+use App\Entity\Category;
+use App\WebContent\Component;
+use App\Entity\MediaObjectImage;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+
+
+
+class ComponentImagesType extends AbstractType
+{
+    private $slugger;
+
+    private $entityManager;
+
+    private $componentService;
+
+    public function __construct(EntityManagerInterface $entityManager, Component $componentService)
+    {
+        $this->slugger = new AsciiSlugger();
+        $this->entityManager = $entityManager;
+        $this->componentService = $componentService;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $views = $this->componentService->getComponentViews();
+
+        $builder
+            ->add('_slug', TextType::class, [
+                'disabled' => true,
+                'data' => (isset($options['data']['slug']))? $options['data']['slug']: '',
+                'label' => 'app.ui_element.field.slug',
+                'mapped' => false,
+                'help' => 'This field will be automatically edited',
+                'required' => false,
+            ])
+            ->add('slug', HiddenType::class, [
+                'disabled' => false,
+            ])
+            ->add('designation', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['groups' => ['component_contact_form_validation']])
+                ],
+                'label' => 'app.ui_element.field.designation',
+            ])
+            ->add('images', EntityType::class, [
+                'class'         => MediaObjectImage::class,
+                'expanded'      => false,
+                'multiple'      => true,
+                'placeholder' => 'app.ui_element.field.select_option',
+                'attr' => ['class' => 'select2-image'],
+            ])
+        ;
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event): void {
+            $data = $event->getData();
+            $form = $event->getForm();
+            if(empty($data['slug'])) {
+                $string = $form->getConfig()->getName() . ' ' . $data['designation'];
+                $data['slug'] = $this->slugger->slug($string)->lower()->toString();
+            }
+            unset($data['_slug']);
+            $event->setData($data);
+        });
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'validation_groups' => ['component_images_validation'],
+        ]);
+    }
+}
