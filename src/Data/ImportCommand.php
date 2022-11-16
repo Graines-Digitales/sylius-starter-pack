77d4ed2137ea524f03eabc431960f8e386ee838d
Johan Remy <johan.remy@graines-digitales.online>
2022-11-16 20:24:34 +0100

    import images

--- src/Data/ImportCommand.php
+++ src/Data/ImportCommand.php
@@ -26,7 +26,7 @@ class ImportCommand extends Import
         $svgsPath = $kernelProjectDir . '/content/svgs';
 
         $filesystem = new Filesystem();
-        if(!$filesystem->exists($svgsPath)) {
+        if($filesystem->exists($svgsPath)) {
             $finder = new Finder();
             $finder->files()->in($imagesPath);
             if ($finder->hasResults()) {
@@ -88,7 +88,7 @@ class ImportCommand extends Import
         }
 
         $filesystem = new Filesystem();
-        if(!$filesystem->exists($svgsPath)) {
+        if($filesystem->exists($svgsPath)) {
             $finder = new Finder();
             $finder->files()->in($svgsPath);
             if ($finder->hasResults()) {
