41e3417d60ce8d679b7ecd4c98ed526e1ccaf37b
Johan Remy <johan.remy@graines-digitales.online>
2022-12-05 13:16:36 +0100

    corrections diverses

--- src/Controller/Api/PaymentController.php
+++ src/Controller/Api/PaymentController.php
@@ -257,7 +257,7 @@ class PaymentController extends AbstractController
             "vads_cust_country" => $data['addressCountry'],
             "vads_cust_phone" => $data['phone'],
             "vads_cust_email" => $data['email'],
-            "vads_url_return" => 'https://preprod.kazengarden.com/booking_confirmation',
+            "vads_url_return" => $this->getParameter('web_host'),
             "vads_redirect_success_timeout" => 10
         ];
     }
