ebeffa730b741cbe0f5ff67c6140b398e110ea10
Johan Remy <johan.remy@graines-digitales.online>
2022-12-06 20:06:22 +0100

    corrections diverses

--- src/Form/Type/DiscountCodeType.php
+++ src/Form/Type/DiscountCodeType.php
@@ -2,15 +2,18 @@
 
 namespace App\Form\Type;
 
+use App\Entity\Trip;
 use App\Entity\DiscountCode;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 
 class DiscountCodeType extends AbstractType
 {
@@ -59,6 +62,22 @@ class DiscountCodeType extends AbstractType
                 'attr_translation_parameters' => [
                     'translatable' => false
                 ]
+            ])
+            ->add('trips', EntityType::class, [
+                'required' => false,
+                // 'attr' => ['class' => 'select2-standard'],
+                'class'         => Trip::class,
+                'expanded'      => true,
+                'multiple'      => true,
+                'by_reference' => false,
+                // 'placeholder' => 'app.ui_element.field.select_option',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                      
+                        ->where('c.arrivalTime > :now')
+                        ->setParameter('now', new \DateTime() )
+                    ;
+                }
             ])
         ;
     }
